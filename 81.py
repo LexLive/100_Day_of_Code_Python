6a78275492408b628fec93626547ce29613e09fc
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-02 18:55:52 +0700

    Update 81.py

--- 81.py
+++ 81.py
@@ -1,7 +1,7 @@
 import math
 
 def paint_calc(height, width, cover):
-    area = height * width4
+    area = height * width
     num_of_cans = math.ceil(area / cover)
     print(f"You'll need {num_of_cans} can of paint.")
 
