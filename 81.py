93ed577246977ccb9bcbdecf842e12dcb13ee6e6
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-02 18:44:41 +0700

    Create 81.py

---
+++ 81.py
@@ -0,0 +1,10 @@
+import math
+
+def paint_calc(height, width, cover):
+    print(math.ceil(height * width / coverage))
+
+
+test_h = int(input("Height: "))
+test_w = int(input("Width: "))
+coverage = 5
+paint_calc(height = test_h, width=test_w, cover=coverage)
