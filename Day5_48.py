35b03872291843271885adc0f7e197f4a98312b8
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-02 19:42:52 +0700

    Update Day5_48.py

--- Day5_48.py
+++ Day5_48.py
@@ -1,6 +1,9 @@
 #Using the for loop with Python list
 
+#Using the for loop with Python Lists
 basket = ["Apple", "Peach", "Pear"]
 
 for fruits in basket:
     print(fruits)
+    print(fruits + " Pie")
+print(basket)
